8ae6ad55cd65796c4f66428a5715122c5fed27ec
andre <andrelucenag10@gmail.com>
2024-06-25 22:36:36 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -17,17 +18,16 @@ public class Main {
         curso2.setDescricao("descrição curso js");
         curso2.setCargaHoraria(4);
 
-        Mentoria mentoria = new Mentoria();
 
+        Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
         System.out.println(curso1);
-
         System.out.println(curso2);
-
         System.out.println(mentoria);
+
 
 
 
