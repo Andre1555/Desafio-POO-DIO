bb7e7715946a986eb10408712a82238c94d745fe
andre <andrelucenag10@gmail.com>
2024-06-26 16:40:26 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -24,9 +22,49 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devAndre = new Dev();
+        devAndre.setNome("Andre");
+        devAndre.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos André:" + devAndre.getConteudosInscritos());
+
+        devAndre.progredir();
+        devAndre.progredir();
+
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos André:" + devAndre.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos André:" + devAndre.getConteudosConcluidos());
+        System.out.println("XP:" + devAndre.calcularXp());
+
+        System.out.println("--------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
+
+        devJoao.progredir();
+        devJoao.progredir();
+        devJoao.progredir();
+
+
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos João:" + devJoao.getConteudosConcluidos());
+        System.out.println("XP:" + devJoao.calcularXp());
+
+        System.out.println("Parabéns, você concluiu todos os conteúdos do Bootcamp!");
+
 
 
 
